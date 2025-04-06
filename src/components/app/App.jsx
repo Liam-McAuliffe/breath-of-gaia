73686745b13e15df55134fe94ef9fc0737fa8ac8
Liam-McAuliffe <lmmcauliffe1223@gmail.com>
2025-04-06 19:12:43 -0400

    init

---
+++ src/components/app/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import LocationForm from '../locationForm/LocationForm';
+
+function App() {
+  return (
+    <div className="app-container">
+      <LocationForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/locationForm/LocationForm.jsx
@@ -0,0 +1,5 @@
+const LocationForm = () => {
+  return <div className="location-form"></div>;
+};
+
+export default LocationForm;
